77bcd8b447368c8934ca6a99836b2268e1596bfe
joelmcdaniel <joelmcdbme@gmail.com>
2026-02-06 22:19:43 -0800

    refactor tests for Sum, SumAll, and SumAllTails functions for clarity and consistency

--- arrays/sum.go
+++ arrays/sum.go
@@ -6,3 +6,23 @@ func Sum(numbers []int) (sum int) {
 	}
 	return sum
 }
+
+func SumAll(numbersToSum ...[]int) (sums []int) {
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+	return sums
+}
+
+func SumAllTails(numbersToSum ...[]int) (sums []int) {
+	for _, numbers := range numbersToSum {
+		l := len(numbers)
+		if l == 0 {
+			sums = append(sums, l)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+	return sums
+}
