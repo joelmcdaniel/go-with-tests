c77b8f46829b4b054e0200689af82e2a9621c625
joelmcdaniel <joelmcdbme@gmail.com>
2026-02-06 18:07:25 -0800

    remove commented-out assertion function from addr_test.go

--- integers/addr_test.go
+++ integers/addr_test.go
@@ -12,16 +12,7 @@ func TestAddr(t *testing.T) {
 	if sum != expected {
 		t.Errorf("expected '%d' but got '%d'", expected, sum)
 	}
-
-	//assertCorrectMessage(t, expected, sum)
 }
-
-/* func assertCorrectMessage(t testing.TB, expected, got int) {
-	t.Helper()
-	if got != expected {
-		t.Errorf("expected '%d' but got '%d'", expected, got)
-	}
-} */
 
 func ExampleAdd() {
 	sum := Add(1, 5)
