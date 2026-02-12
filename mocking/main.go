55a785c6fb9250eacc008f44bc8cf22625f74165
joelmcdaniel <joelmcdbme@gmail.com>
2026-02-12 00:03:50 -0800

    refactor Countdown and Sleeper implementations for improved testability

--- mocking/main.go
+++ mocking/main.go
@@ -14,14 +14,17 @@ type Sleeper interface {
 	Sleep()
 }
 
-type DefaultSleeper struct{}
+type ConfigurableSleeper struct {
+	duration time.Duration
+	sleep    func(time.Duration)
+}
 
-func (d *DefaultSleeper) Sleep() {
-	time.Sleep(1 * time.Second)
+func (c *ConfigurableSleeper) Sleep() {
+	c.sleep(c.duration)
 }
 
 func main() {
-	sleeper := &DefaultSleeper{}
+	sleeper := &ConfigurableSleeper{1 * time.Second, time.Sleep}
 	Countdown(os.Stdout, sleeper)
 }
 
